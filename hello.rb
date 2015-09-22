76cc8388564eeabfcfec80c5b4d7a9e96bc524ee
Michael O'Flaherty <mikejof@gmail.com>
2015-09-22 12:51:19 +0930

    Testing git

--- hello.rb
+++ hello.rb
@@ -3,14 +3,14 @@ i = true
 
 while i==true
 
-puts "Enter a number between 1 and 10"
+puts "Enter a number between 1 and 10:"
 number = gets.to_i
 
 if number<=10 and number>=1
 	puts "Valid number"
 	i = false
 else
-	puts "Please reenter a number between 1 and 10"
+	puts "Please reenter a number between 1 and 10:"
 
 end 
 	
